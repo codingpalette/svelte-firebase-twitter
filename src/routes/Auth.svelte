8398a49215ee979e150737bdcaa1ced2f5522377
codingpalette <codingpalette@gmail.com>
2020-10-07 12:39:08 +0900

    user store 생성

--- src/routes/Auth.svelte
+++ src/routes/Auth.svelte
@@ -1,5 +1,13 @@
 <script>
+    import { onMount } from 'svelte';
+    import { isLoggedIn } from '../store/user';
+    import { push } from 'svelte-spa-router';
 
+    onMount(() => {
+        if($isLoggedIn) {
+            push('/')
+        }
+    })
 </script>
 
 <div>
