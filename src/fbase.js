8950658cd7858410d613eb6d801563804d23bf89
codingpalette <codingpalette@gmail.com>
2020-10-12 15:04:12 +0900

    file upload 작업

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app'
+import 'firebase/firebase-auth'
+import 'firebase/firebase-firestore'
+import 'firebase/firebase-storage'
 
 const firebaseConfig = {
     apiKey: __myapp.env.API_KEY,
@@ -18,3 +19,4 @@ export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
 export const dbService = firebase.firestore();
+export const storageService = firebase.storage();
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,7 +1,8 @@
 <script>
     import { onMount } from 'svelte';
     import { push } from 'svelte-spa-router';
-    import { authService, dbService } from '../fbase';
+    import { v4 as uuidv4 } from 'uuid';
+    import { authService, dbService, storageService } from '../fbase';
     import { currentUser } from '../store/user';
     import Tweet from '../components/Tweet.svelte';
 
@@ -15,6 +16,7 @@
 
     let tweet = '';
     let tweets = [];
+    let attachment = '';
 
     const getTweets = async () => {
         const dbTweets = await dbService.collection('tweets').get();
@@ -36,17 +38,36 @@
                 ...doc.data()
             }))
             tweets = tweetArray;
-            console.log(tweetArray)
+            // console.log(tweetArray)
         })
-    })
+    });
+
+    const onFileChange = (event) => {
+        const { target: { files }} = event;
+        const theFile = files[0];
+        // console.log(theFile);
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const { currentTarget : { result } } = finishedEvent
+            attachment = result;
+        }
+        reader.readAsDataURL(theFile);
+    }
+
+    const onClearAttachment = () => attachment = null;
 
     const onSubmit = async () => {
-        await dbService.collection('tweets').add({
-            text: tweet,
-            createAt: Date.now(),
-            creatorId: $currentUser.uid,
-        });
-        tweet = '';
+        const fileRef = storageService.ref().child(`${$currentUser.uid}/${uuidv4()}`)
+        const response = await fileRef.putString(attachment, "data_url");
+        console.log(response)
+        // await dbService.collection('tweets').add({
+        //     text: tweet,
+        //     createAt: Date.now(),
+        //     creatorId: $currentUser.uid,
+        // });
+        // tweet = '';
+
+
     }
 </script>
 
@@ -54,7 +75,14 @@
 <div>
     <form on:submit|preventDefault={onSubmit}>
         <input type="text" placeholder="무슨일이 있는지 적어주세요." maxlength="120" bind:value={tweet} />
-        <input type="submit" value="Tweet">
+        <input type="file" accept="image/*" on:change={onFileChange} />
+        <input type="submit" value="Tweet" />
+        {#if attachment}
+            <div>
+                <img src={attachment} width="50px" height="50px" />
+                <button on:click={onClearAttachment}>Clear</button>
+            </div>
+        {/if}
     </form>
     <div>
         {#each tweets as data}
